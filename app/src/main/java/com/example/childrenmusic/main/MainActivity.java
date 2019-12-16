987e3c2f4863d6538f66810547d318032c6679e2
Aida <a.mosayebi478@gmail.com>
2019-12-16 12:10:27 +0330

    Interner Connection

--- app/src/main/java/com/example/childrenmusic/main/MainActivity.java
+++ app/src/main/java/com/example/childrenmusic/main/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.childrenmusic.main;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
@@ -27,5 +31,36 @@ public class MainActivity extends AppCompatActivity {
         MainAdapter adapter=new MainAdapter(this, MainDataGenarator.getMainDataModel(this));
         recyclerView.setLayoutManager(new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.HORIZONTAL));
         recyclerView.setAdapter(adapter);
+
+        boolean isConnected=isNetworkConnected();
+        if (!isConnected)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("اتصال به اینترنت");
+            builder.setIcon(R.drawable.wifi);
+            builder.setMessage("برای پخش موسیقی نیاز به اتصال به اینترنت داریم");
+
+         /*   builder.setPositiveButton("بله", new DialogInterface.OnClickListener() {
+                @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+                public void onClick(DialogInterface dialog, int which) {
+                    finishAffinity();
+                }
+            });*/
+
+            builder.setNegativeButton("باشه", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+    }
+
+    public boolean isNetworkConnected()
+    {
+        ConnectivityManager cm= (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
     }
 }
